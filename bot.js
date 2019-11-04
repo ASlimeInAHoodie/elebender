008a40cfc123dbf25d05cafaba56a1e10285dda6
ASlimeInAHoodie <57351844+ASlimeInAHoodie@users.noreply.github.com>
2019-11-05 07:39:51 +1100

    Added env prefix variable
    process.env.PREFIX

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-
+const prefix = process.env.PREFIX;
  
 
 client.on('ready', () => {
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
+client.login(process.env.BOT_TOKEN);
