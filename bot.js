00d481cbbfe5903fff31b7d59cb15610b30987c4
ASlimeInAHoodie <57351844+ASlimeInAHoodie@users.noreply.github.com>
2019-11-05 07:44:35 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const prefix = string(process.env.PREFIX);
+const prefix = process.env.PREFIX;
  
 
 client.on('ready', () => {
