393a420245d7a15c309ea0e09d283ece1c40a27b
ASlimeInAHoodie <57351844+ASlimeInAHoodie@users.noreply.github.com>
2019-11-05 17:50:05 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = process.env.PREFIX;
 const spreadsheetId = process.env.SHEETID;
- 
+const readline = require('readline');
+const {google} = require('googleapis');
+
 
 client.on('ready', () => {
 
