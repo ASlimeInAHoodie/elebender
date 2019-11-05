2d913b7ccfc499aa6a3321cd6379ffc08ddf050b
ASlimeInAHoodie <57351844+ASlimeInAHoodie@users.noreply.github.com>
2019-11-05 15:32:36 +1100

    Update bot.js
    added env var: SHEETID

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 const prefix = process.env.PREFIX;
+const spreadsheetId = process.env.SHEETID;
  
 
 client.on('ready', () => {
