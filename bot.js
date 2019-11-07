3a9df36b1d3592888bf9c1f44b6b3471daed0ea4
ASlimeInAHoodie <57351844+ASlimeInAHoodie@users.noreply.github.com>
2019-11-07 13:13:03 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('message', message => {
         
                //message.reply('invalid username');
                 client.sendMessage({
-                    to: message.channelID,
+                    to: message.channel,
                     message: "Invalid Username"
                 });
            
