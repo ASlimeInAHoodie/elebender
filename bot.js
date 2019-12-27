beed0f2fec84fc4e4efca9ba4bd01644944d1221
ASlimeInAHoodie <57351844+ASlimeInAHoodie@users.noreply.github.com>
2019-12-27 11:59:04 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,11 +14,11 @@ const CODE7 = process.env.CODE7;
 const CODE8 = process.env.CODE8;
 const CODE9 = process.env.CODE9;
 const CODE10 = process.env.CODE10;
-def login = "none";
-def admin = False;
 
 client.on('ready', () => {
     console.log('I am ready!');
+    login = "none";
+    admin = False;
 });
 
  
